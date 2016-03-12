c2e7de670b30ff192e39384aeb91393be6e39049
hancharh <hancharh@go.stockton.edu>
2016-03-12 00:09:29 -0500

    Lab stuff

---
+++ Music/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        //[Required(ErrorMessage = "Name of genre is required")]
+        //[MaxLength(20, ErrorMessage = "Name must be shorter than 20 characters")]
+        public string Name { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+}
